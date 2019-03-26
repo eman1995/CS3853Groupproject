a0a6a3a2de1823331ba915c76248497d0e128df5
gzm916 <gzm916@my.utsa.edu>
2019-03-26 14:21:03 -0500

    maths

--- sim.py
+++ sim.py
@@ -9,6 +9,14 @@ import re
 
 #We do len(sys.argv - 1) because we dont care about sim.py (sim.py –f trace1.txt –s 1024 –b 16 –a 2 –r RR)
 #10 being 10 arguments
+
+def powers(x):
+    i = 0
+    while x != 1 and x >= 1:
+        x /= 2
+        i += 1
+    return i
+
 if (len(sys.argv) - 1) == 10:
     
     #for every other argument not including (sim.py) in the command line Ex. (–f) trace1.txt (–s) 1024 (–b) 16 (–a) 2 (–r) RR
@@ -75,6 +83,19 @@ if (len(sys.argv) - 1) == 10:
         elif 'r' in arg:
             replacement_policy = sys.argv[10]
 
+    totaladdrspaceb = 32 #addr space in bits assumed to be 32
+    c2 = powers(int(cache_size)) + 10 #cache in KB i.e. 1024 = 20
+    b2 = powers(int(block_size)) #bytes per block in 2 i.e. 16 = 4
+    blksin2 = (c2 - b2) # #of blocks in 2 (2^20)/(2^4) = 2^16 or 16
+    blksin2b = blksin2 - 10 # # of blocks rep without kilobytes 16 - 10 = 2^6
+    numblksKB = 2**blksin2b
+    #offsetb = totaladidspaceb - b2 # example should be 32 - 4 = 28 bits remain in addr space
+    abinbits = (b2 + powers(int(associativity))) #offset = block size plus associativity in bits i.e. 4 + 1 (blk size + associativty in powers of two) 
+    index = (c2 - abinbits) #cache size / blocksize * associativity 
+    tindicies = 2**(index - 10) #totalindicies is given in KB simeply converting
+    tagbits = totaladdrspaceb - (b2 + index) #addrspace - (offset bits + index bits) goes up with associativity
+    overhead = ((tagbits + 1) * (2**blksin2))/8 #(tagbits + valid bits) * (# of blks) / 8
+    imp = overhead + (2**c2) # overhead plus cache size
     # Print results
     print("\nCache Simulator CS 3853 Spring 2019 - Group #21")
     print("")
@@ -92,13 +113,15 @@ if (len(sys.argv) - 1) == 10:
     print("Policy:", replacement_policy)
     print("")
     print("----- Calculated Values -----")
-    print("Total #Blocks:", 64, "KB", "(2^16)")
-    print("Tag Size:", 13,"bits")
-    print("Index Size:", 15, "bits,", "Total Indices:", 32, "KB")
-    print("Implementation Memory Size:", "114,688", "bytes")
+    print("Total #Blocks: {} KB (2^{})".format(numblksKB, blksin2))
+    print("Tag Size: {} bits".format(tagbits))
+    print("Index Size: {} bits, Total Indices: {} KB".format(index, tindicies))
+    print("Overhead Size: {:,} bytes".format(int(overhead)))
+    print("Implementation Memory Size: {:,} bytes".format(int(imp)))
     print("")
     print("----- Results -----")
-    print("Cache Hit Rate:", 96.2,"%")
+    print("Cache Hit Rate: {}%".format(96.2))
+    print("Cache Miss Rate: {}%".format(3.8))
 
 else:
     print("Error: Argument(s) not in range.")
